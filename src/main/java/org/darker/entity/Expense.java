1f3ff4ba9a92ddbc4fbc550ec7833c5e6f696ee1
Darker <bhosalesudarshan9@gmail.com>
2025-03-03 18:40:04 +0530

    created structure

---
+++ src/main/java/org/darker/entity/Expense.java
@@ -0,0 +1,25 @@
+package org.darker.entity;
+
+import org.darker.enums.ExpenseCategory;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="expenses")
+public class Expense {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private ExpenseCategory category;
+	
+	@Column(nullable = false)
+	private double amount;
+}
---
+++ src/main/java/org/darker/entity/Savings.java
@@ -0,0 +1,49 @@
+package org.darker.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="savings")
+public class Savings {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@Column(nullable = false)
+	private double balance;
+
+	public Savings() {
+		
+	}
+	public Savings(User user, double balance) {
+		
+		this.user = user;
+		this.balance = balance;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+	
+}

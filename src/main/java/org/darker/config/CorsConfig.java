87e24c6c49cc496cfc048c3e5b78c77ecae95ac4
Darker <bhosalesudarshan9@gmail.com>
2025-03-15 13:06:55 +0530

    deployment process

--- src/main/java/org/darker/config/CorsConfig.java
+++ src/main/java/org/darker/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOriginPatterns("http://localhost:5173")
+				registry.addMapping("/api/**").allowedOriginPatterns("http://localhost:5173","https://ets-darker.vercel.app")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
 						.allowCredentials(true);
 			}
